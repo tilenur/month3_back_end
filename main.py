de8cde5b68b6de33c07e6bca07b8d2b33595c70f
tilenur <tilenur@gmail.com>
2026-03-13 18:26:30 +0600

    hw4 done

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ import flet as ft
 
 from datetime import datetime
 
-
 def main(page: ft.Page):
   page.title = "Homework for month 3"
   page.theme_mode = ft.ThemeMode.DARK
-
   text_hello = ft.Text(value="Привет")
-
   greetings_history = []
-
   history_text = ft.Text("Greetings History")
-
-
-
 
   def on_click_func(_):
     name = name_input.value 
-
     current_time = datetime.now()
     formatted_time = current_time.strftime("%Y:%m:%d - %H:%M:%S")
 
@@ -45,25 +37,43 @@ def main(page: ft.Page):
       page.theme_mode = ft.ThemeMode.DARK
     page.update()
 
+
+  def delete_last_func(_):
+    if greetings_history:
+      greetings_history.pop()
+      history_text.value = "Greetings history:\n" + "\n".join(greetings_history)
+    else:
+      history_text.value = "История пуста!"
+    page.update()
+
+  def sort_alphabet_func(_):
+    if greetings_history:
+      greetings_history.sort(
+        key=lambda item: item.split("Привет, ")[1]
+      )
+      history_text.value = "Greetings history:\n" + "\n".join(greetings_history)
+    else:
+      history_text.value = "История пуста!"
+    page.update()
+
   def reset_click(_):
     name_input.value = ""
     text_hello.value = "Привет"
     history_text.value = "Greetings history:\n"
 
     greetings_history.clear()
-    page.update()
+    page.update()  
 
 
   name_input = ft.TextField(label="Insert name", expand=True, on_submit=on_click_func)
-
   send_button = ft.Button("send", icon=ft.Icons.SEND, color=ft.Colors.YELLOW, icon_color=ft.Colors.GREEN, on_click=on_click_func)
-
   theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=edit_theme)
-
   reset_button = ft.Button("reset", icon=ft.Icons.DELETE, color=ft.Colors.YELLOW, icon_color=ft.Colors.RED, on_click=reset_click)
+  delete_last_button = ft.Button("Delete last", icon=ft.Icons.DELETE_OUTLINE, color=ft.Colors.RED, icon_color=ft.Colors.GREEN, on_click=delete_last_func)
+  sort_alphabet_button = ft.Button("Sort by alphabet", icon=ft.Icons.SORT, color=ft.Colors.GREEN, on_click=sort_alphabet_func)
 
 
-  main_objects = ft.Row([name_input, send_button, theme_button, reset_button])
+  main_objects = ft.Row([name_input, send_button, theme_button, reset_button, delete_last_button, sort_alphabet_button])
 
   page.add(text_hello, main_objects, history_text)
 
